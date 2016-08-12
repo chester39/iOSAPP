b68adf15e7d6afc0b19966eaaf917c1d6636574d
chester39 <chester39@163.com>
2016-08-12 19:21:44 +0800

    add CCQQFramework

---
+++ CCQQFramework/CCQQFramework/ViewController.m
@@ -0,0 +1,72 @@
+//
+//	iOS培训
+//		传智播客 & 黑马
+//		Chen Chen @ June 16th, 2015
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+#pragma mark - 系统方法
+
+/**
+ *  视图已经加载方法
+ */
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+#pragma mark - UITableViewDataSource数据源方法
+
+/**
+ *  共有组数方法
+ */
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+/**
+ *  每组行数方法
+ */
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+/**
+ *  每行内容方法
+ */
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 0;
+}
+
+#pragma mark - Modal方法
+
+/**
+ *  改变分段卡值方法
+ */
+- (IBAction)changeSegmented:(UISegmentedControl *)sender
+{
+    if (sender.selectedSegmentIndex == 1) {
+        UIViewController *vcA = [[UIViewController alloc] init];
+        vcA.view.backgroundColor = [UIColor blueColor];
+        [self presentViewController:vcA animated:YES completion:^{
+            NSLog(@"Modal打开");
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [self dismissViewControllerAnimated:YES completion:^{
+                    NSLog(@"Modal关闭");
+                }];
+            });
+        }];
+    }
+}
+
+@end
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # iOS APP
 ## By Chester Chen
 ## Some iOS APP by Chester Chen, learning from the videos of Itcast Inc.
-## July 18th, 2016 - August 11th, 2016
+## July 18th, 2016 - August 12th, 2016
 
 ### Contents
 
@@ -24,3 +24,4 @@
 - [APP 17. 导航使用](CCNavigationUse)
 - [APP 18. 数据存储](CCDataStorage)
 - [APP 19. 个人通讯录](CCPersonalContacts)
+- [APP 20. QQ框架](CCQQFramework)
